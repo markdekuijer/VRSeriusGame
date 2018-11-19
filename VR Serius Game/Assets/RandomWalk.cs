4f226de49d0019f09138886a5d3059dd1858479b
markdekuijer <31616400+markdekuijer@users.noreply.github.com>
2018-11-19 15:17:27 +0100

    very basic prototype

---
+++ VR Serius Game/Assets/RandomWalk.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RandomWalk : MonoBehaviour
+{
+    NavMeshAgent agent;
+    float maxTime = 8f;
+    float currentTime;
+
+    public Shop shop;
+
+    public int iterationsInShop;
+    bool leaving;
+    [SerializeField] bool willBuySomething;
+
+	void Start ()
+    {
+        iterationsInShop = Random.Range(3, 7);
+        agent = GetComponent<NavMeshAgent>();
+        currentTime = maxTime;
+        agent.SetDestination(GetNewLocationInShop());
+        int i = Random.Range(0, 2);
+        if (i == 0)
+            willBuySomething = false;
+        else
+            willBuySomething = true;
+    }
+
+    void Update ()
+    {
+        if (iterationsInShop <= 0)
+            return;
+
+        currentTime -= Time.deltaTime;
+        if(currentTime <= 0)
+        {
+            agent.SetDestination(GetNewLocationInShop());
+            iterationsInShop--;
+            currentTime = maxTime;
+            if (iterationsInShop <= 0)
+            {
+                if (willBuySomething)
+                {
+                    shop.AddToQueue(this);
+                }
+                else
+                {
+                    Leave();
+                }
+            }
+
+        }
+    }
+
+    public void Leave()
+    {
+        agent.SetDestination(shop.GetShopExit());
+        leaving = true;
+    }
+
+    public void SetQueuePositions(Transform t)
+    {
+        agent.SetDestination(t.position);
+    }
+
+    public Vector3 GetNewLocationInShop()
+    {
+
+        return shop.GetNewLocationInShop();
+    }
+}
---
+++ VR Serius Game/Assets/Shop.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public float shopRadius;
+    public GameObject shopExit;
+
+    List<RandomWalk> walkers = new List<RandomWalk>();
+    public List<Transform> queuePositions = new List<Transform>();
+
+    public float maxTimer = 1;
+    float currentTimer;
+
+    public int queueIteration;
+
+    private void Start()
+    {
+        currentTimer = maxTimer;
+    }
+
+    private void Update()
+    {
+        if(walkers.Count > 0)
+        {
+            currentTimer -= Time.deltaTime;
+            if(currentTimer <= 0)
+            {
+                walkers[0].Leave();
+                walkers.RemoveAt(0);
+                queueIteration--;
+                for (int i = 0; i < walkers.Count; i++)
+                {
+                    walkers[i].SetQueuePositions(queuePositions[i]);
+                }
+                currentTimer = maxTimer;
+            }
+        }
+    }
+
+    public Vector3 GetNewLocationInShop()
+    {
+        Vector3 newLocation = Random.insideUnitSphere * shopRadius;
+        newLocation.y = 0;
+        return newLocation;
+    }
+
+    public Vector3 GetShopExit()
+    {
+        return new Vector3(shopExit.transform.position.x, 0, shopExit.transform.position.z);
+    }
+
+    public void AddToQueue(RandomWalk walker)
+    {
+        walker.SetQueuePositions(queuePositions[queueIteration]);
+        walkers.Add(walker);
+        queueIteration++;
+    }
+}
