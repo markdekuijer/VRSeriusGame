c32ed48de5ad2582bf8a3d5b506dde4b9c36fa31
markdekuijer <31616400+markdekuijer@users.noreply.github.com>
2019-01-21 12:26:15 +0100

    goodbye steamvr

--- VR Serius Game/Assets/Code/CharacterInput.cs
+++ VR Serius Game/Assets/Code/CharacterInput.cs
@@ -2,18 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using Valve.VR;
-using Valve.VR.InteractionSystem;
+//using Valve.VR;
+//using Valve.VR.InteractionSystem;
 
 public class CharacterInput : MonoBehaviour
 {
-    public SteamVR_Action_Boolean testTriggerAction;
-    public Hand rightHand;
+    //public SteamVR_Action_Boolean testTriggerAction;
+    //public Hand rightHand;
 
 	void Update ()
     {
-        if (Test())
-            print("UseSecond");
+        //if (Test())
+        //    print("UseSecond");
         //if (IsEligible(rightHand))
         
 
@@ -24,51 +24,51 @@ public class CharacterInput : MonoBehaviour
     public void CheckPointing()
     {
         Unit u = null;
-        if (Test())
-        {
-            Debug.DrawRay(rightHand.gameObject.transform.position, rightHand.gameObject.transform.forward * 100);
-            RaycastHit hit;
-            if (Physics.Raycast(rightHand.gameObject.transform.position, rightHand.gameObject.transform.forward, out hit, Mathf.Infinity))
-            {
-                if (hit.transform.gameObject.CompareTag("Unit"))
-                {
-                    u = hit.transform.GetComponent<Unit>();
-                    u.ChangeColor(true);
-                }
-            }
+        //if (Test())
+        //{
+            //Debug.DrawRay(rightHand.gameObject.transform.position, rightHand.gameObject.transform.forward * 100);
+            //RaycastHit hit;
+            //if (Physics.Raycast(rightHand.gameObject.transform.position, rightHand.gameObject.transform.forward, out hit, Mathf.Infinity))
+            //{
+            //    if (hit.transform.gameObject.CompareTag("Unit"))
+            //    {
+            //        u = hit.transform.GetComponent<Unit>();
+            //        u.ChangeColor(true);
+            //    }
+            //}
 
-        }
-        else if (u != null)
-        {
-            u.ChangeColor(false);
-            u = null;
-        }
+        //}
+        //else if (u != null)
+        //{
+        //    u.ChangeColor(false);
+        //    u = null;
+        //}
             
     }
 
-    public bool Test()
-    {
-        return testTriggerAction.GetState(rightHand.handType);
-    }
+    //public bool Test()
+    //{
+    //    return testTriggerAction.GetState(rightHand.handType);
+    //}
 
-    public bool IsEligible(Hand hand)
-    {
-        if (hand == null)
-            return false;
+    //public bool IsEligible(Hand hand)
+    //{
+    //    if (hand == null)
+    //        return false;
 
-        if (!hand.gameObject.activeInHierarchy)
-            return false;
+    //    if (!hand.gameObject.activeInHierarchy)
+    //        return false;
 
-        if (hand.hoveringInteractable != null)
-            return false;
+    //    if (hand.hoveringInteractable != null)
+    //        return false;
 
-        if (hand.noSteamVRFallbackCamera == null)
-        {
-            if (hand.isActive == false)
-            {
-                return false;
-            }
-        }
-        return true;
-    }
+    //    if (hand.noSteamVRFallbackCamera == null)
+    //    {
+    //        if (hand.isActive == false)
+    //        {
+    //            return false;
+    //        }
+    //    }
+    //    return true;
+    //}
 }
